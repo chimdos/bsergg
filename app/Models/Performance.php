f60a6c4f21919dd1804f664ab0ddf0f789374652
chimdos <d4ni.martis.24@gmail.com>
2026-01-03 15:45:04 -0300

    feat: adiciona o model de Performance

--- app/Models/Performance.php
+++ app/Models/Performance.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Performance extends Model
 {
-    //
+    protected $fillable = [
+        'game_id', 'player_id', 'brawler_name', 'kills', 'deaths', 
+        'damage_dealt', 'damage_received', 'healing', 'damage_to_safe', 
+        'rating_bser', 'is_win'
+    ];
+
+    public function player(): BelongsTo
+    {
+        return $this->belongsTo(Player::class);
+    }
+
+    public function game(): BelongsTo
+    {
+        return $this->belongsTo(Game::class);
+    }
 }
